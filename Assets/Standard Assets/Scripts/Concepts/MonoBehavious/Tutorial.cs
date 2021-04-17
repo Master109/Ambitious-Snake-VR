fcba9e661a518994f02fea526286a510fb166777
Gilead Cosman <gileadcosman@gmail.com>
2021-04-17 13:42:09 -0700

    Finished SetDominantHandTutorial.cs

--- Assets/Standard Assets/Scripts/Concepts/MonoBehavious/Tutorial.cs
+++ Assets/Standard Assets/Scripts/Concepts/MonoBehavious/Tutorial.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace AmbitiousSnake
 {
-	public class Tutorial : UpdateWhileEnabled
+	public class Tutorial : SingletonUpdateWhileEnabled<Tutorial>
 	{
+		public PlayableDirector playableDirector;
+		public GameObject[] activateOnFinish;
+
 		public static bool IsLookingAtTransform (Transform trs, float shrinkCameraViewNormalized)
 		{
 			Vector3 viewportPoint = Camera.main.WorldToViewportPoint(trs.position);
 			shrinkCameraViewNormalized /= 2;
 			return viewportPoint.x >= shrinkCameraViewNormalized && viewportPoint.x <= 1f - shrinkCameraViewNormalized && viewportPoint.y >= shrinkCameraViewNormalized && viewportPoint.y <= 1f - shrinkCameraViewNormalized;
+		}
+
+		public virtual void Finish ()
+		{
+			gameObject.SetActive(false);
+			for (int i = 0; i < activateOnFinish.Length; i ++)
+			{
+				GameObject go = activateOnFinish[i];
+				go.SetActive(true);
+			}
 		}
 	}
 }
---
+++ Assets/Standard Assets/Scripts/Concepts/MonoBehavious/Tutorials/SetDominantHandTutorial.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using Extensions;
+
+namespace AmbitiousSnake
+{
+	public class SetDominantHandTutorial : Tutorial
+	{
+		public PlayableAsset playWhileLookingAt0Hands;
+		public PlayableAsset playWhileLookingAt1Hand;
+		public PlayableAsset playWhileLookingAt2Hands;
+		public float shrinkCameraViewNormalized;
+		public static bool LeftHandIsDominant
+		{
+			get
+			{
+				return PlayerPrefsExtensions.GetBool("Left-handed", false);
+			}
+			set
+			{
+				PlayerPrefsExtensions.SetBool("Left-handed", value);
+			}
+		}
+		bool lookingAtLeftHand;
+		bool lookingAtRightHand;
+		bool previousLookingAtLeftHand;
+		bool previousLookingAtRightHand;
+
+		public override void OnEnable ()
+		{
+			base.OnEnable ();
+			playableDirector.stopped += OnPlayableDirectorStopped;
+		}
+
+		void OnPlayableDirectorStopped (PlayableDirector playableDirector)
+		{
+			if (lookingAtLeftHand != lookingAtRightHand && lookingAtLeftHand == previousLookingAtLeftHand && lookingAtRightHand == previousLookingAtRightHand)
+				Finish ();
+		}
+
+		public override void Finish ()
+		{
+			base.Finish ();
+			LeftHandIsDominant = lookingAtLeftHand;
+		}
+
+		public override void OnDisable ()
+		{
+			base.OnDisable ();
+			playableDirector.stopped -= OnPlayableDirectorStopped;
+		}
+
+		public override void DoUpdate ()
+		{
+			lookingAtLeftHand = IsLookingAtTransform(VRCameraRig.instance.leftHandTrs, shrinkCameraViewNormalized);
+			lookingAtRightHand = IsLookingAtTransform(VRCameraRig.instance.rightHandTrs, shrinkCameraViewNormalized);
+			if (lookingAtLeftHand != previousLookingAtLeftHand || lookingAtRightHand != previousLookingAtRightHand)
+			{
+				playableDirector.Stop();
+				if (lookingAtLeftHand != lookingAtRightHand)
+					playableDirector.Play(playWhileLookingAt1Hand);
+				else if (lookingAtLeftHand && lookingAtRightHand)
+					playableDirector.Play(playWhileLookingAt2Hands);
+				else
+					playableDirector.Play(playWhileLookingAt0Hands);
+			}
+			previousLookingAtLeftHand = lookingAtLeftHand;
+			previousLookingAtRightHand = lookingAtRightHand;
+		}
+	}
+}
