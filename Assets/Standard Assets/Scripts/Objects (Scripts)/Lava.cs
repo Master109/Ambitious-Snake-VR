ee147b717768b8d94160d30380bb4bd63049efad
Gilead Cosman <gileadcosman@gmail.com>
2021-04-23 04:35:52 -0700

    Added sound effects when objects drop into lava

--- Assets/Standard Assets/Scripts/Objects (Scripts)/Lava.cs
+++ Assets/Standard Assets/Scripts/Objects (Scripts)/Lava.cs
@@ -6,8 +6,13 @@ namespace AmbitiousSnake
 {
 	public class Lava : Hazard
 	{
+		public AudioClip[] onTriggerEnterAudioClips = new AudioClip[0];
+		public AnimationCurve onTriggerEnterSpeedToVolumeCurve;
+
 		void OnTriggerEnter (Collider other)
 		{
+			float volume = onTriggerEnterSpeedToVolumeCurve.Evaluate(other.GetComponentInParent<Rigidbody>().velocity.magnitude);
+			AudioManager.instance.MakeSoundEffect (onTriggerEnterAudioClips[Random.Range(0, onTriggerEnterAudioClips.Length)], other.GetComponent<Transform>().position, volume);
 			IDestructable destructable = other.GetComponentInParent<IDestructable>();
 			if (destructable != null)
 				ApplyDamage (destructable, damage);
