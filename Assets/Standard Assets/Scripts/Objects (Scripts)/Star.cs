c45e0a512796115d9ea1cd34e0bfd39b9b17e3a5
Gilead Cosman <gileadcosman@gmail.com>
2021-04-23 04:40:44 -0700

    Made sound effect for picking up stars

--- Assets/Standard Assets/Scripts/Objects (Scripts)/Star.cs
+++ Assets/Standard Assets/Scripts/Objects (Scripts)/Star.cs
@@ -5,12 +5,15 @@ namespace AmbitiousSnake
 {
 	public class Star : SingletonMonoBehaviour<Star>
 	{
+		public Transform trs;
 		public bool isCollected;
+		public AudioClip[] onTriggerEnterAudioClips = new AudioClip[0];
 
 		void OnTriggerEnter (Collider other)
 		{
 			isCollected = true;
 			gameObject.SetActive(false);
+			AudioManager.instance.MakeSoundEffect (onTriggerEnterAudioClips[Random.Range(0, onTriggerEnterAudioClips.Length)], trs.position);
 		}
 	}
 }
