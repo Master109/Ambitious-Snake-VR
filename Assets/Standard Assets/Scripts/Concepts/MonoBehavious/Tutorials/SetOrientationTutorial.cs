4341de308afe0dde4edd212b8acc76e311cb9eb1
Gilead Cosman <gileadcosman@gmail.com>
2021-04-23 04:13:10 -0700

    Modified tutorials for level 1 and 2

--- Assets/Standard Assets/Scripts/Concepts/MonoBehavious/Tutorials/SetOrientationTutorial.cs
+++ Assets/Standard Assets/Scripts/Concepts/MonoBehavious/Tutorials/SetOrientationTutorial.cs
@@ -5,9 +5,20 @@ namespace AmbitiousSnake
 {
 	public class SetOrientationTutorial : Tutorial
 	{
+		public GameObject leftThumbstickIndicator;
+		public GameObject rightThumbstickIndicator;
 		public Vector2 maxCameraForwardToFinish;
 		bool setOrientationInput;
 		bool previousSetOrientationInput;
+
+		public override void OnEnable ()
+		{
+			base.OnEnable ();
+			if (!SetDominantHandTutorial.LeftHandIsDominant)
+				leftThumbstickIndicator.SetActive(true);
+			else
+				rightThumbstickIndicator.SetActive(true);
+		}
 		
 		public override void DoUpdate ()
 		{
@@ -15,6 +26,13 @@ namespace AmbitiousSnake
 			if (setOrientationInput && VRCameraRig.instance.eyesTrs.forward.x <= maxCameraForwardToFinish.y && VRCameraRig.instance.eyesTrs.forward.y <= maxCameraForwardToFinish.y)
 				Finish ();
 			previousSetOrientationInput = setOrientationInput;
+		}
+
+		public override void Finish ()
+		{
+			base.Finish ();
+			leftThumbstickIndicator.SetActive(false);
+			rightThumbstickIndicator.SetActive(false);
 		}
 	}
 }
